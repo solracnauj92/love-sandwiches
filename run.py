eb18238f604041aa21ea87748512b2bb44ff3695
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2023-03-16 19:27:12 +0000

    Test

--- run.py
+++ run.py
@@ -1,135 +1,65 @@
-import gspread
-from google.oauth2.service_account import Credentials
+import sqlite3
+import random
+import string
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+# Connect to the database
+conn = sqlite3.connect('passwords.db')
+c = conn.cursor()
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+# Create the table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS passwords
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              website TEXT,
+              username TEXT,
+              password TEXT)''')
 
+# Function to generate a random password
+def generate_password(length=10):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
 
-def get_sales_data():
-    """
-    Get sales figures input from the user.
-    Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated
-    by commas. The loop will repeatedly request data, until it is valid.
-    """
-    while True:
-        print("Please enter sales data from the last market.")
-        print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
+# Function to add a password to the database
+def add_password(website, username, password):
+    c.execute('INSERT INTO passwords (website, username, password) VALUES (?, ?, ?)',
+              (website, username, password))
+    conn.commit()
 
-        data_str = input("Enter your data here:\n")
+# Function to get a password from the database
+def get_password(website):
+    c.execute('SELECT password FROM passwords WHERE website = ?', (website,))
+    password = c.fetchone()
+    if password:
+        return password[0]
+    else:
+        return None
 
-        sales_data = data_str.split(",")
+# Menu function
+def menu():
+    print("1. Generate a password for a new website")
+    print("2. Get a password for an existing website")
+    print("3. Quit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        website = input("Enter the website: ")
+        username = input("Enter the username: ")
+        password = generate_password()
+        print("Generated password:", password)
+        add_password(website, username, password)
+    elif choice == "2":
+        website = input("Enter the website: ")
+        password = get_password(website)
+        if password:
+            print("Password:", password)
+        else:
+            print("No password found for", website)
+    elif choice == "3":
+        print("Goodbye!")
+        conn.close()
+        quit()
+    else:
+        print("Invalid choice, try again.")
+    menu()
 
-        if validate_data(sales_data):
-            print("Data is valid!")
-            break
-
-    return sales_data
-
-
-def validate_data(values):
-    """
-    Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values.
-    """
-    try:
-        [int(value) for value in values]
-        if len(values) != 6:
-            raise ValueError(
-                f"Exactly 6 values required, you provided {len(values)}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-        return False
-
-    return True
-
-
-def update_worksheet(data, worksheet):
-    """
-    Receives a list of integers to be inserted into a worksheet
-    Update the relevant worksheet with the data provided
-    """
-    print(f"Updating {worksheet} worksheet...\n")
-    worksheet_to_update = SHEET.worksheet(worksheet)
-    worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully\n")
-
-
-def calculate_surplus_data(sales_row):
-    """
-    Compare sales with stock and calculate the surplus for each item type.
-    The surplus is defined as the sales figure subtracted from the stock:
-    - Positive surplus indicates waste
-    - Negative surplus indicates extra made when stock was sold out.
-    """
-    print("Calculating surplus data...\n")
-    stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock[-1]
-    
-    surplus_data = []
-    for stock, sales in zip(stock_row, sales_row):
-        surplus = int(stock) - sales
-        surplus_data.append(surplus)
-
-    return surplus_data
-
-
-def get_last_5_entries_sales():
-    """
-    Collects columns of data from sales worksheet, collecting
-    the last 5 entries for each sandwich and returns the data
-    as a list of lists.
-    """
-    sales = SHEET.worksheet("sales")
-
-    columns = []
-    for ind in range(1, 7):
-        column = sales.col_values(ind)
-        columns.append(column[-5:])
-
-    return columns
-
-
-def calculate_stock_data(data):
-    """
-    Calculate the average stock for each item type, adding 10%
-    """
-    print("Calculating stock data...\n")
-    new_stock_data = []
-
-    for column in data:
-        int_column = [int(num) for num in column]
-        average = sum(int_column) / len(int_column)
-        stock_num = average * 1.1
-        new_stock_data.append(round(stock_num))
-
-    return new_stock_data
-
-
-def main():
-    """
-    Run all program functions
-    """
-    data = get_sales_data()
-    sales_data = [int(num) for num in data]
-    update_worksheet(sales_data, "sales")
-    new_surplus_data = calculate_surplus_data(sales_data)
-    update_worksheet(new_surplus_data, "surplus")
-    sales_columns = get_last_5_entries_sales()
-    stock_data = calculate_stock_data(sales_columns)
-    update_worksheet(stock_data, "stock")
-
-
-print("Welcome to Love Sandwiches Data Automation")
-main()
+# Start the program
+menu()
